afb402e394bbd97699fe6d43a6e6559b57a495ef
jason <pochli6262@gmail.com>
2025-02-26 13:30:36 +0800

    Initial Commit

---
+++ First_iOS_APP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  First_iOS_APP
+//
+//  Created by 廖柏鈞 on 2025/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ First_iOS_APP/First_iOS_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_iOS_APPApp.swift
+//  First_iOS_APP
+//
+//  Created by 廖柏鈞 on 2025/2/26.
+//
+
+import SwiftUI
+
+@main
+struct First_iOS_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
